6cded7cae606eac522f6f4a5c4d6649fcf4f917c
Austin Friedrich <friedrau@users.noreply.github.com>
2023-11-11 03:11:12 -0800

    Work In Progress
    11/10/2023 Update

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ function myTestFunction() {
 
 /*toggle between hiding and showing the dropdown content */
 function dropDownMenuToggle() {
-    document.getElementById("mainMenuDropDown").classList.toggle("show");
+    var myDropdown = document.getElementById("mainMenuDropDown");
+    if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+    }
+    else if (!myDropdown.classList.contains('show')) {
+        document.getElementById("mainMenuDropDown").classList.toggle("show");
+    }
+    
 }
-
+// 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("mainMenuDropDown");
-      if (myDropdown.classList.contains('show')) {
+        if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
-      }
+        }
     }
-  }
+}
 
-  
