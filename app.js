1ab568648bdea0d292399893b1c873caee9bdca3
Austin Friedrich <friedrau@users.noreply.github.com>
2023-11-12 02:15:48 -0800

    Animated Header and final layout

--- app.js
+++ app.js
@@ -36,3 +36,23 @@ window.onresize = function resizeCheck(){
         //    alert("window width >= 700px");
     }
 }
+
+
+// Scroll down animation
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {navbarScrollDown()};
+
+// Get the header
+var header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the solid class to the header when you reach its scroll position. Remove "solid" when you leave the scroll position
+function navbarScrollDown() {
+  if (window.scrollY > sticky) {
+    header.classList.add("solid");
+  } else {
+    header.classList.remove("solid");
+  }
+}
