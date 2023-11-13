03fe2e90dcb8ff272a94db4ac9bf8c823cd13177
Austin Friedrich <friedrau@users.noreply.github.com>
2023-11-13 00:37:21 -0800

    Projects, WorkHistory done.
    Contact page under construction

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php 
 if(isset($_POST['submit'])){
     $to = "austin.finiteprobability@gmail.com"; // this is your Email address
@@ -15,20 +16,6 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
     }
-// Example HTML
 ?>
-<!--  -->
-<!-- <!DOCTYPE html> -->
-<!-- <head> -->
-<!-- <title>Form submission</title> -->
-<!-- </head> -->
-<!-- <body> -->
-<!--  -->
-<!-- <form action="" method="post"> -->
-<!-- First Name: <input type="text" name="first_name"><br> -->
-<!-- Last Name: <input type="text" name="last_name"><br> -->
-<!-- Email: <input type="text" name="email"><br> -->
-<!-- Message:<br><textarea rows="5" name="message" cols="30"></textarea><br> -->
-<!-- <input type="submit" name="submit" value="Submit"> -->
-<!-- </form> -->
